a600b588198707907c7aae2371e5e368d8b8df47
ShanuMaurya <36756132+ShanuMaurya@users.noreply.github.com>
2018-10-13 18:09:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ client.on('ready', () => {
     client.user.setActivity('Sρυυкιι Pεтяσℓ シ', {type: 'WATCHING'});
 });
 
-client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+client.on("message", (message) => {
+  if(message.content === "hug") {
+    message.channel.send(" *hugs you_!*");
+  }
 });
 
 client.login(process.env.TOKEN);
